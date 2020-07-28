95dd6122b37beb3a9c6335a4dcc8ba66cb7aa37e
Grant Sowards <gsowards100@gmail.com>
2020-07-28 03:33:12 +0000

    commit-bot

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import time, subprocess
+import time, subprocess, random
 while True:
     with open('/home/pi/code/commit-bot/.commit-bot.txt','a') as file:
-        file.write(' ' * 5)
+        file.write('0' * random.randint(0,100))
         file.close()
-    time.sleep(5)
+    #time.sleep(3600 * 12)
 
     subprocess.run(['git','add','.'])
     subprocess.run(['git', 'commit', '-m', 'commit-bot'])
     subprocess.run(['git', 'push'])
+    time.sleep(3600*12)
